0ebbee4998591a2c445cc4a5962cec116bead640
Mobina <MobinaSafari1414@gmail.com>
2024-06-24 20:15:25 +0330

    first view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def login(request):
+    if request.method=='GET':
+        context={}
+        render(request,'login.html',context)
+def register(request):
+    pass
+
+
+    
